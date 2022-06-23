10d864536765c8d61303027b950bd14dc7d08e32
Agung Wicaksono <agungwck@landx.id>
2022-06-23 20:04:28 +0700

    [ADD] Invoice model

---
+++ moka_python_sdk/models/transaction/invoice/entity/invoice.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from typing import List
+
+from moka_python_sdk.models.transaction.invoice import invoice
+
+@dataclass
+class InvoiceData:
+    id: str
+    status: str
+    payment_no: str
+    payment_type: str
+    amount_pay: int
+    amount_change: int
+    total_discount_amount: int
+    total_gratuity_amount: int
+    total_item_price_amount: int
+    total_tax_amount: int
+    total_checkouts_amount: int
+    is_refunded: bool
+    refunded_reason: str
+    refunded_date: str
+    business_id: int
+    customer_id: int
+    discount_id: int
+    is_deleted: bool
+    created_at: str
+    updated_at: str
+    total_custom_price_amount: int
+    include_gratuity_tax: bool
+    parent_payment_id: str
+    refund_amount: int
+    refund_type: str
+    card_no: str
+    created_by: int
+    note: str
+    total_collected_amount: int
+    server_id: int
+    server_name: str
+    server_title: str
+    auth_code: str
+    cc_name: str
+    card_type: str
+    transaction_number: str
+    transaction_reference: str
+    outlet_id: int
+    guid: str
+    synchronized_at: str
+    uniq_id: str
+    transaction_certificate: str
+    transaction_status_info: str
+    merchant_id: int
+    mpos_device_id: int
+    pg_mid: str
+    pg_setting: str
+    order_info: str
+    tvr: str
+    cvm_result: str
+    aid: str
+    transaction_date: str
+    pii: str
+    collected_by: str
+    shift_id: int
+    invoice_no: str
+    invoice_deposit_amount: int
+    invoice_due_date: str
+    invoice_status: str
+    latitude: float
+    longitude: float
+    refund_ids: list
+    total_gross_sales: int
+    total_net_sales: int
+    is_refund_breakdown: bool
+    customer_name: str
+    customer_email: str
+    customer_phone: str
+    subtotal: int
+    creator_name: str
+    table_name: str
+    collector_id: int
+    cashlezz_transaction_id: str
+    sms_id: str
+    receipt_count: int
+    order_id: str
+    is_loyalty: bool
+    total_redeem_amount: int
+    is_offline: bool
+    is_sales_type: bool
+    server_created_at: str
+    total_rounding_amount: int
+    bill_id: str
+    tax_type: str
+    gratuity_type: str
+    uuid: str
+    parent_payment_uuid: str
+
+
+@dataclass
+class InvoiceEntity:
+    next_url: str
+    completed: bool
+    invoice: List[InvoiceData]
---
+++ moka_python_sdk/models/transaction/invoice/invoice.py
@@ -0,0 +1,76 @@
+from moka_python_sdk._api_requestor import _APIRequestor
+from moka_python_sdk.moka_error import MokaError
+from models._to_model import _to_model
+
+from .entity.invoice import InvoiceEntity, InvoiceData
+
+class Invoice:
+    @staticmethod
+    def report(
+        *,
+        outlet_id: str,
+        per_page: int = 10,
+        page: int = 1,
+        start: float = None,
+        end: float = None,
+        time_filter: str = None,
+        reorder_type: str = None,
+    ) -> InvoiceEntity:
+        """Send GET Request to Get invoices.
+        (API Reference : https://api.mokapos.com/docs#operation/reportInvoicesV3)
+
+        Args:
+            - outlet_id (str): ID of the outlet that will be searched.
+            - per_page (int): Number of items per page.
+            - page (int): Page number.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+            - time_filter (str): Type of fitler to be used by start and end parameters {created_at, updated_at, synchronized_at}
+            - reorder_type (str): Sort results in ascending or descending order {ASC, DESC}
+
+        Returns:
+            data: Array invoices
+        """
+        url = f"/v3/outlets/{outlet_id}/reports/invoices"
+        body = {
+            "per_page": per_page,
+            "page": page,
+            "start": start,
+            "end": end,
+            "time_filter": time_filter,
+            "reorder_type": reorder_type,
+        }
+        response = _APIRequestor.get(url, body=body)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=InvoiceEntity, data=response.body)
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def report_detail(
+        *,
+        outlet_id: str,
+        invoice_id: str,
+    ) -> InvoiceEntity:
+        """Send GET Request to Get invoice detail.
+        (API Reference : https://api.mokapos.com/docs#operation/reportInvoiceDetailV3)
+
+        Args:
+            - outlet_id (str): ID of the outlet that will be searched.
+            - invoice_id (str): ID of the invoice that will be searched.
+        
+        Returns:
+            data: Invoice detail
+        """
+        url = f"/v3/outlets/{outlet_id}/invoices/{invoice_id}"
+        body = {
+            "invoice_id": invoice_id,
+        }
+        response = _APIRequestor.get(url, body=body)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=InvoiceData, data=response.body)
+        else:
+            raise MokaError(response)
+        
+
+
