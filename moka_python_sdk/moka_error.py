3e920b979953746f45a1edc0ff9269a31dcbdaba
Agung Wicaksono <agungwck@landx.id>
2022-06-23 15:07:03 +0700

    update error handling

--- moka_python_sdk/moka_error.py
+++ moka_python_sdk/moka_error.py
@@ -2,10 +2,9 @@ class MokaError(Exception):
     """Error that will be given when status code != 200."""
 
     def __init__(self, response):
-        super(MokaError, self).__init__(response.body["errors"][0]["detail"])
+        super(MokaError, self).__init__(response.body["meta"]["error_message"])
 
-        error = response.body["errors"][0]
-        self.status_code = response.status_code
-        self.error_code = error["code"]
-        self.title = error["title"]
-        self.errors = response.body.get("errors", None)
+        meta = response.body["meta"]
+        self.status_code = meta["code"]
+        self.error_code = meta["error_type"]
+        self.title = meta["error_message"]
