1f3c8fabc63789bd48511a8802418ac8d1e668ff
Agung Wicaksono <agungwck@landx.id>
2022-06-30 08:17:25 +0700

    add item model

---
+++ moka_python_sdk/models/transaction/entity/item.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class ItemEntity:
+    item_id: int
+    item_name: str
+    quantity: int
+    item_variant_id: int
+    item_variant_name: str
+    note: str
+    item_price_library: int
+    category_id: int
+    category_name: str
