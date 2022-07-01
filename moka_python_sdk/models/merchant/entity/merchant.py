f62b047c0dc8f5371d4015bab47779b705364bef
Agung Wicaksono <agungwck@landx.id>
2022-07-01 14:17:46 +0700

    add merchant model

---
+++ moka_python_sdk/models/merchant/entity/merchant.py
@@ -0,0 +1,181 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class BusinessDetail:
+    id: int
+    name: str
+    address: str
+    suite: str
+    city: str
+    province: str
+    postal_code: str
+    description: str
+    email: str
+    phone: str
+    logo: str
+    notify_per_deposit: str
+    notify_per_trans: str
+    user_id: int
+    is_deleted: bool
+    created_at: str
+    updated_at: str
+    business_type_id: int
+    business_category_id: int
+    website: str
+    twitter: str
+    facebook: str
+    instagram: str
+    notes: str
+    latitude: float
+    longitude: float
+    synchronized_at: str
+    outlet_slot: int
+    is_trial: bool
+    current_plan: int
+    expired_at: str
+    last_payment_date: str
+    trial_end_at: str
+
+
+@dataclass
+class BusinessInfo:
+    business: BusinessDetail
+
+@dataclass
+class BusinessSetting:
+    enable_tax: bool
+    enable_gratuity: bool
+    include_gratuity_tax: bool
+    enable_rounding: bool
+
+@dataclass
+class LoyaltyCustomer:
+    point_balance: int
+    join_date: str
+
+@dataclass
+class CustomerDetail:
+    id: int
+    email: str
+    name: str
+    phone: str
+    address: str
+    city: str
+    state: str
+    postal_code: str
+    business_id: int
+    birthday: str
+    is_deleted: bool
+    created_at: str
+    updated_at: str
+    synchronized_at: str
+    loyalty: LoyaltyCustomer
+
+@dataclass
+class CustomerEntity:
+    customer: CustomerDetail
+
+@dataclass
+class EarningRule:
+    id: int
+    type: str
+    point: int
+    amount: int
+
+@dataclass
+class RewardItem:
+    id: int
+    type: str
+    category_name: str
+    item_name: str
+    item_variant_names: List[str]
+
+@dataclass
+class Reward:
+    id: int
+    reward_type: str
+    discount: int
+    discount_type: str
+    max_discount_amount: int
+    min_purchase_amount: int
+    created_at: str
+    created_by: str
+    updated_at: str
+    updated_by: str
+    deleted_at: str
+    deleted_by: str
+    synchronized_at: str
+    reward_items: List[RewardItem]
+    redeem_point: int
+    is_deleted: bool
+
+
+@dataclass
+class LoyaltyEntity:
+    id: int
+    business_id: int
+    new_member_point: int
+    program_revision: int
+    status: str
+    inactive_since: str
+    reset_since: str
+    created_at: str
+    created_by: str
+    updated_at: str
+    updated_by: str
+    deleted_at: str
+    deleted_by: str
+    synchronized_at: str
+    is_sent_validation_code: bool
+    earning_rules: List[EarningRule]
+    rewards: List[dict]
+    is_deleted: bool
+
+@dataclass
+class OutletDetail:
+    id: int
+    business_id: int
+    name: str
+    address: str
+    phone_number: str
+    city: str
+    province: str
+    postal_code: str
+    latitude: float
+    longitude: float
+    is_deleted: bool
+    created_at: str
+    updated_at: str
+    synchronized_at: str
+    importing: bool
+    exporting: bool
+    notes: str
+    start_date: str
+    end_date: str
+    is_paying: bool
+
+
+@dataclass
+class OutletEntity:
+    outlets: List[OutletDetail]
+    total_count: int
+
+@dataclass
+class UserInfo:
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    full_name: str
+    business_id: int
+    business_name: str
+    outlet_ids: List[int]
+    outlet_names: List[str]
+
+@dataclass
+class UserQuota:
+    application_id: str
+    resource: str
+    quota: int
+    refresh_at: str
---
+++ moka_python_sdk/models/merchant/merchant.py
@@ -0,0 +1,147 @@
+from os import stat
+from moka_python_sdk._api_requestor import _APIRequestor
+from moka_python_sdk.moka_error import MokaError
+from models._to_model import _to_model
+from typing import List
+
+from .entity.merchant import (
+    BusinessInfo, BusinessSetting, CustomerEntity,
+    LoyaltyEntity, OutletEntity, UserInfo,
+    UserQuota
+)
+
+class Merchant:
+    @staticmethod
+    def show_business_info(
+        *,
+        **kwargs
+    ) -> BusinessInfo:
+        """
+        Returns the merchant's business information
+        (API Reference: https://api.mokapos.com/docs#operation/showBusinessInformationV1)
+        """
+        url = "v1/businesses"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=BusinessInfo, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def get_business_checkout_setting(
+        *,
+        business_id: int,
+        **kwargs
+    ) -> BusinessInfo:
+        """
+        Retrieve the checkout settings of a business
+        (API Reference: https://api.mokapos.com/docs#operation/getBusinessCheckoutSettingsV1)
+        """
+        url = f"v1/businesses/{business_id}/checkout_settings"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=BusinessSetting, data=response.body['data'])
+        else:
+            raise MokaError(response)
+
+    @staticmethod
+    def get_list_customer(
+        *,
+        business_id: int,
+        mobile_device: int,
+        page: int,
+        per_page: int,
+        since: float,
+        until: float,
+        include_deleted: bool,
+        include_loyalty: bool,
+        **kwargs
+    ) -> CustomerEntity:
+        """
+        Returns all customers on the outlet that the user has access to
+        (API Reference: https://api.mokapos.com/docs#operation/listCustomersV1)
+        """
+        url = f"v1/businesses/{business_id}/customers"
+        params = {
+            "mobile_device": mobile_device,
+            "page": page,
+            "per_page": per_page,
+            "since": since,
+            "until": until,
+            "include_deleted": include_deleted,
+            "include_loyalty": include_loyalty
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=CustomerEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_loyalty_program(
+        *,
+        business_id: int,
+        **kwargs
+    ) -> List[LoyaltyEntity]:
+        """
+        Returns all the loyalties programs of a merchant
+        (API Reference: https://api.mokapos.com/docs#operation/showLoyaltyProgramV1)
+        """
+        url = f"v1/businesses/{business_id}/loyalties/programs"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=List[LoyaltyEntity], data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def get_list_outlets(
+        *,
+        business_id: int,
+        **kwargs
+    ) -> OutletEntity:
+        """
+        Returns current user outlets information
+        (API Reference: https://api.mokapos.com/docs#operation/listOutletsInformationV1)
+
+        """
+        url = f"v1/businesses/{business_id}/outlets"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=OutletEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_user_info(
+        *,
+        **kwargs
+    ) -> UserInfo:
+        """
+        Returns current user information that logged in
+        (API Reference: https://api.mokapos.com/docs#operation/showUserInformation)
+
+        """
+        url = "v1/profile/self"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=UserInfo, data=response.body)
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_quota(
+        *,
+        **kwargs
+    ) -> UserQuota:
+        """
+        Returns remaining quota of an outlet when calling certain API.
+        (API Reference: https://api.mokapos.com/docs#operation/showQuotaInformationV1)
+
+        """
+        url = "v1/quotas"
+        response = _APIRequestor.get(url, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=UserQuota, data=response.body["data"])
+        else:
+            raise MokaError(response)
