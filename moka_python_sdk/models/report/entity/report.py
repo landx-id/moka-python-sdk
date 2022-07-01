50c3c2a8d3b632e572c5d132255633e52a1b3f3c
Agung Wicaksono <agungwck@landx.id>
2022-07-01 09:24:51 +0700

    add report model

---
+++ moka_python_sdk/models/report/entity/report.py
@@ -0,0 +1,176 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class CategorySalesDetail:
+    name: str
+    items_sold: int
+    gross_sales: float
+    discount: float
+    refund: float
+    net_sales: float
+
+@dataclass
+class CategorySalesEntity:
+    category_sales: List[CategorySalesDetail]
+    total_items_sold: int
+    total_gross_sales: float
+    total_discounts: float
+    total_refunds: float
+    total_net_sales: float
+
+@dataclass
+class ReportDetail:
+    name: str
+    title: str
+    number_of_transactions: int
+    total: float
+
+@dataclass
+class CollectorSalesEntity:
+    total_number_of_transactions: int
+    grand_total: float
+    reports: List[ReportDetail]
+
+@dataclass
+class ModifierOption:
+    option_id: int
+    option_name: str
+    quantity: int
+    gross_sales: float
+    discount: float
+    refund: float
+    net_sales: float
+
+
+@dataclass
+class ModifierDetail:
+    modifier_id: int
+    modifier_name: str
+    quantity: int
+    gross_sales: float
+    discount: float
+    refund: float
+    net_sales: float
+    modifier_options: List[ModifierOption]
+
+
+@dataclass
+class ModifierSalesEntity:
+    modifier: List[dict]
+    total_quantity: int
+    total_gross_sales: float
+    total_discounts: float
+    total_refunds: float
+    total_net_sales: float
+
+@dataclass
+class DiscountDetail:
+    discount_id: int
+    name: str
+    amount: float
+    discount_applied: float
+    count: int
+    discount_type: str
+    refund: float
+    net_discount: float
+
+@dataclass
+class DiscountSalesEntity:
+    discount: List[DiscountDetail]
+    total_count: int
+    total_discounts_applied: float
+    total_refunds: float
+
+@dataclass
+class ReportTax:
+    name: str
+    amount: float
+    taxable_amount: float
+    tax_collected: float
+
+@dataclass
+class TaxSalesEntity:
+    report_taxes: List[ReportTax]
+    total_tax_collected: float
+
+@dataclass
+class ReportGratuity:
+    name: str
+    amount: int
+    gratuity_collected: float
+
+@dataclass
+class GratuitySalesEntity:
+    reports_gratuities: List[ReportGratuity]
+    total_gratuity_collected: float
+
+@dataclass
+class ServerDetail:
+    server_id: int
+    server_name: str
+    server_title: str
+    number_transaction: int
+    collected: int
+
+@dataclass
+class ServerSalesEntity:
+    server: List[ServerDetail]
+    total_collected: float
+    total_number_of_transactions: int
+
+@dataclass
+class ItemSalesDetail:
+    name: str
+    sku: str
+    category_name: str
+    item_sold: int
+    item_refunded: int
+    gross_sales: float
+    discount: float
+    refund: float
+    net_sales: float
+    cogs: float
+    redemption: float
+    gross_profit: float
+
+
+@dataclass
+class ItemSalesEntity:
+    item_sales: List[dict]
+    total_item_sold: int
+    total_gross_sales: float
+    total_discounts: float
+    total_net_sales: float
+    total_refund: float
+
+@dataclass
+class SalesSummaryEntity:
+    gross_sales: float
+    discounts: float
+    refunds: float
+    net_sales: float
+    gratuities: float
+    taxes: float
+    total_collected: float
+    number_of_transactions: int
+    rounding_amount: float
+    redemption: float
+
+@dataclass
+class ReportObject:
+    payment_type: str
+    number_of_transactions: int
+    collected: float
+
+@dataclass
+class ReportDetail:
+    cash_or_card: List[ReportObject]
+    invoice: List[ReportObject]
+    other: List[ReportObject]
+
+@dataclass
+class PaymentMethodsEntity:
+    total_number_of_transactions: int
+    total_collected: float
+    reports: List[ReportDetail]
---
+++ moka_python_sdk/models/report/report.py
@@ -0,0 +1,352 @@
+from moka_python_sdk._api_requestor import _APIRequestor
+from moka_python_sdk.moka_error import MokaError
+from models._to_model import _to_model
+from typing import List
+
+from .entity.report import (
+    CategorySalesEntity, CollectorSalesEntity, ModifierSalesEntity,
+    DiscountSalesEntity, TaxSalesEntity, GratuitySalesEntity,
+    ServerSalesEntity, ItemSalesEntity, SalesSummaryEntity,
+    PaymentMethodsEntity
+)
+
+class Report:
+    @staticmethod
+    def show_category_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> CategorySalesEntity:
+        """Send GET Request to Show category sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#tag/Category-Sales)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Category data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/category_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=CategorySalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def get_collector_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> CollectorSalesEntity:
+        """Send GET Request to Show collector sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showCollectorSalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Collector data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/collector_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=CollectorSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_modifier_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> ModifierSalesEntity:
+        """Send GET Request to Show modifier sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showModifierSalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Modifier data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/modifier_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=ModifierSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_discount_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> DiscountSalesEntity:
+        """Send GET Request to Show discount sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showDiscountSalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Discount data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/discount_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=DiscountSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+
+    @staticmethod
+    def show_tax_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> TaxSalesEntity:
+        """Send GET Request to Show tax sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showTaxSalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Tax data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/tax_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=TaxSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+
+    @staticmethod
+    def show_gratuity_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> GratuitySalesEntity:
+        """Send GET Request to Show gratuity sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showGratuitySalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Gratuity data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/gratuity_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=GratuitySalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+        
+    @staticmethod
+    def show_server_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> ServerSalesEntity:
+        """Send GET Request to Show server sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showServerSalesV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Server data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/server_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=ServerSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+    
+    @staticmethod
+    def show_item_sales(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> ItemSalesEntity:
+        """Send GET Request to Get Show item sales for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showItemSalesV3)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Item data
+        """
+        url = f"/v3/outlets/{outlet_id}/reports/item_sales"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=ItemSalesEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+
+    @staticmethod
+    def show_sales_summary(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> SalesSummaryEntity:
+        """Send GET Request to Show sales summary for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showSalesSummaryV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Sales summary data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/sales_summary"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=SalesSummaryEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
+
+    @staticmethod
+    def show_payment_methods(
+        *,
+        outlet_id: int,
+        per_page: int,
+        start: float,
+        end: float,
+        **kwargs
+    ) -> PaymentMethodsEntity:
+        """Send GET Request to Show payment methods for selected date range on the selected outlet.
+        (API Reference : https://api.mokapos.com/docs#operation/showPaymentMethodsV2)
+
+        Args:
+            - outlet_id (int): ID of the outlet that will be searched.
+            - per_page (int): Number of elements per page.
+            - start (float): Start date in DD/MM/YYYY format
+            - end (float): End date in DD/MM/YYYY format
+
+        Returns:
+            data: Payment methods data
+        """
+        url = f"/v2/outlets/{outlet_id}/reports/payment_methods"
+
+        params = {
+            "per_page": per_page,
+            "start": start,
+            "end": end,
+        }
+        response = _APIRequestor.get(url, params=params, **kwargs)
+        if response.status_code >= 200 and response.status_code < 300:
+            return _to_model(model=PaymentMethodsEntity, data=response.body['data'])
+        else:
+            raise MokaError(response)
